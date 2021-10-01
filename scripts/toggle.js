62392212ef9be878c5d6cef1559aa657e0a9dc65
Shrivathsa <svpred99@gmail.com>
2021-10-01 19:59:46 +0530

    added theme toggle

---
+++ scripts/toggle.js
@@ -0,0 +1,34 @@
+var text_color =       "#bbbbbb";
+var background_color = "#180018";
+var text_color_light = "#111";
+var background_color_light = "#dadada";
+
+function change_theme(){
+	if (document.getElementsByTagName('body')[0].classList.contains('dark-theme')){
+		localStorage['svok33-github-theme'] = 'light';
+		document.getElementById('svok33-change-theme').textContent = "Dark theme"
+	}
+	else{
+		localStorage['svok33-github-theme'] = 'dark';
+		document.getElementById('svok33-change-theme').textContent = "Light theme"
+	}
+
+	apply_theme();
+}
+
+function apply_theme(){
+	if(localStorage['svok33-github-theme'] == 'dark'){
+		document.getElementsByTagName('body')[0].classList.remove('light-theme');
+		document.getElementsByTagName('body')[0].classList.add('dark-theme');
+
+		//set background colour for triggered hamburger menu
+		//there is only one such div whose background colour needs to be changed
+		document.getElementsByClassName('trigger')[0].style.backgroundColor = background_color;
+	}
+	else{
+		document.getElementsByTagName('body')[0].classList.remove('dark-theme');
+		document.getElementsByTagName('body')[0].classList.add('light-theme');
+
+		document.getElementsByClassName('trigger')[0].style.backgroundColor = background_color_light;
+	}
+}
